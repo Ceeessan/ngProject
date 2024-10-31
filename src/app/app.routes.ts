8c7d98f4690c1607b9615219ac8c4facfbd4bfb7
Ceeessan <cecilia.carlsen@dise.com>
2024-10-31 16:05:51 +0100

    wip(login.component):add new component to login on the CMS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { HomeComponent } from './home/home.component';
 import { ContentComponent } from './content/content.component';
 import { PlaylistComponent } from './playlist/playlist.component';
 import { PlayerDeviceComponent } from './player-device/player-device.component';
-
-
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path: "home", component: HomeComponent, title: "Homepage"},
     {path: "content", component: ContentComponent, title: "content"},
     {path: "playlist", component: PlaylistComponent , title: "playlist"},
     {path: "player-device", component: PlayerDeviceComponent , title: "player-device"},
+    {path: "login", component: LoginComponent , title: "login"},
     {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
